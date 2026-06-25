c42870b7195c089a956457a3a9198e0255a0242b
Manu <manucondev@gmail.com>
2026-06-25 15:28:34 +0200

    restore next config for dynamic deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    devIndicators: false,
-  }
-  
-  export default nextConfig
+  devIndicators: false,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
