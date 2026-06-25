06d4526d44275a17e3ae6c1896b98bed5ea05571
Manu <manucondev@gmail.com>
2026-06-25 15:33:24 +0200

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   devIndicators: false,
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
 }
 
 export default nextConfig
